bfb87d82455f566e4ea18f4d2434dcd9f929e34c
kboateng3 <kboateng3@gmail.com>
2013-11-16 18:59:07 -0500

    final

--- app.rb
+++ app.rb
@@ -10,6 +10,7 @@ enable :sessions
 set :raise_errors, false
 set :show_exceptions, false
 
+enable :sessions
 # Scope defines what permissions that we are asking the user to grant.
 # In this example, we are asking for the ability to publish stories
 # about using the app, access to what the user likes, and to be able
@@ -78,16 +79,17 @@ end
 #############
 get "/" do
   # Get base API Connection
+ 
   @graph  = Koala::Facebook::API.new(access_token)
   @client = Face.get_client(:api_key => 'ad8d512de5354372ba24ea2c328a58c1', :api_secret => '83b9c2f388614e03b2ba72d42fac0e7c')
 
 #debugger
 
-
   # Get public details of current application
   @app  =  @graph.get_object(ENV["FACEBOOK_APP_ID"])
-
   if access_token
+  authenticator.get_user_info_from_cookies(session[:access_token])
+
     @user    = @graph.get_object("me")
     @friends = @graph.get_connections('me', 'friends')
     @photos  = @graph.get_connections('me', 'photos')
@@ -136,10 +138,23 @@ end
   
 def face_check
   #debugger
-   #h  = @client.faces_detect(:urls => ['http://skybiometry.com/Content/Samples/five.jpg'], :attributes => 'all')
+   #h  = @client.faces_detect(:urls => ['http://skybiometry.com/Content/Samples/two.jpg'], :attributes => 'all')
+   #k  = @client.faces_detect(:urls => ['http://skybiometry.com/Content/Samples/one_i.jpg'], :attributes => 'all')
+
+# #of ppl = h['photos'][0]['tags'].length
 
    #h  = @client.faces_detect(:file => File.new('me.jpg', 'rb'), :attributes => 'all')
+   #@user_id = 'acarrington3'
  h  = @client.faces_detect(:urls => ['https://graph.facebook.com/'+@user_id+'/picture?type=large'], :attributes => 'all')
   @features =  h['photos'][0]['tags'][0]['attributes']
 
 end
+
+def number
+
+  
+   h  = @client.faces_detect(:urls => ['http://skybiometry.com/Content/Samples/two.jpg'], :attributes => 'all')
+   result = h['photos'][0]['tags'].length
+   return result
+end
+
